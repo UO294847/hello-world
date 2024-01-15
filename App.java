67ade7d850da81b04708589501087743720ec70f
CNASO76 <UO294847@uniovi.es>
2024-01-15 20:13:46 +0100

    New classes created: alumno & App

---
+++ App.java
@@ -0,0 +1,9 @@
+/**
+ * APP TO TEST MY CLASSES
+ */
+public class App {
+    public static void main(String[] args) {
+        alumno student1 = new alumno("Lucas", 19, "UO908978");
+        System.out.println(student1);
+    }
+}
