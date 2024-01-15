9382b1b487f83ece57b5e2fd105c12d6ae3ebef7
CNASO76 <UO294847@uniovi.es>
2024-01-15 20:28:00 +0100

    Next actualization soon, trying to make a class that stores an ammount of Students

---
+++ Register.java
@@ -0,0 +1,26 @@
+import java.util.Collection;
+import java.util.List;
+
+public class Register<T>{
+    
+    /**
+     * DATA AREA
+     */
+    List<T> data;
+
+    /**
+     * CONSTRUCTORS
+     */
+    public Register(){
+
+    }
+
+    public Register(Collection<? extends T> c){
+        
+    }
+
+    /**
+     * METHODS
+     */
+
+}
