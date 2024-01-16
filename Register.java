737f14fd1c51ba359d4910f5d901f0462275b8c1
CNASO76 <UO294847@uniovi.es>
2024-01-16 20:03:50 +0100

    Implementados los constructores de la clase register. Añadidos algunos metodos tb. Queda bastante por implementar en la clase register

--- Register.java
+++ Register.java
@@ -1,26 +1,64 @@
+import java.util.AbstractCollection;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
-public class Register<T>{
+
+/*
+ * Collection that stores elements in a linked-list. No repeated elements are allowed. They dont have to be ordered. List could be modified
+*/
+public class Register<T> extends AbstractCollection<T>{
     
     /**
      * DATA AREA
      */
-    List<T> data;
+    
+     List<T> data;
 
     /**
      * CONSTRUCTORS
      */
+    
     public Register(){
-
+        this.data = new LinkedList<>();    
     }
 
     public Register(Collection<? extends T> c){
-        
+        this();
+        for(T element : c){
+            if(!this.data.contains(element)) this.data.add(element);
+        }
     }
 
     /**
      * METHODS
      */
+
+    @Override
+    public boolean add(T e){
+        if(this.data.contains(e)){
+            return false;
+        }
+        else{
+            this.data.add(e);
+            return true;
+        }
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    
 
 }
